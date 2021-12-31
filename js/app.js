e17cf488908b28c616f8ee998aedcf8575195a63
Jonathon Lee <jonnyleealas@gmail.com>
2021-12-30 21:40:29 -0700

    table row

--- js/app.js
+++ js/app.js
@@ -1,210 +1,159 @@
 'use strict';
 
-console.log('hi');
-
 const profileContainer = document.getElementById('kittenProfiles');
-
-let jumper = {
-  name: 'Jumper',
-  about: 'Jumper the cat is super sweet!',
-  cutenessIndex: 10,
-  like: [
-    'cuddling',
-    'lazer pointer',
-    'catnap',
-    'catnip'
-  ],
-  render: function () {
-    for (let i in this.like) {
-      // li
-      let li = document.createElement('li');
-      li.textContent = `${this.name} likes ${this.like[i]}`;
-      ul.appendChild(li);
-    }
-  }
-};
+const kittenTable = document.querySelector('table tbody');
 
 let kittenArray = [];
 
-function Kitten(name, about, likes, src) {
+function Kitten(name, about, like, src) {
   this.name = name;
   this.about = about;
-  this.likes = likes;
+  this.cutenessIndex = 10;
+  this.like = like;
   this.src = src;
   this.listId = `${this.name} Likes`;
   this.render = function () {
+    // 1.) create element
     let article = document.createElement('article');
+
+    // 2. or 3.) give it content
+    // 2. or 3.) append it to the DOM
     profileContainer.appendChild(article);
 
-    //h3
+    // h3
     let h3 = document.createElement('h3');
     h3.textContent = this.name;
-    article.appendChild(h3)
-    //p
-    let p = document.createElement('p')
+    article.appendChild(h3);
+
+    // p
+    let p = document.createElement('p');
     p.textContent = this.about;
-    article.appendChild(p)
-    //ul
-    let ul = document.createElement('ul')
-    ul.id = this.listId
-    article.appendChild(ul)
-    //img
-    let img = document.createElement('img')
+    article.appendChild(p);
+
+    // ul
+    let ul = document.createElement('ul');
+    ul.id = this.listId;
+    article.appendChild(ul);
+
+    // img
+    let img = document.createElement('img');
     img.src = this.src;
-    img.about = this.about;
+    img.alt = this.about;
     article.appendChild(img);
 
+    this.renderList();
   };
-  // render
   this.renderList = function () {
-    let kittenLikes = document.getElementById(this.listId);
-    console.log("birch face")
-    for (let i in this.likes) {
+    const kittenLikes = document.getElementById(this.listId);
+    for (let i = 0; i < this.like.length; i++) {
+      // li
       let li = document.createElement('li');
-      li.textContent = `${this.name} likes ${this.likes[i]}`
-      kittenLikes.appendChild(li)
+      li.textContent = `${this.name} likes ${this.like[i]}`;
+      kittenLikes.appendChild(li);
     }
   };
-  console.log('fuck face')
-  kittenArray.push(this)
-};
+  kittenArray.push(this);
+}
 
+// Kitten(name, about, like, src)
+// let jumper = new Kitten(
+new Kitten(
+  'Jumper ',
+  'Jumper the cat is super sweet!',
+  ['cuddling', 'lazer pointer', 'cat nap'],
+  'js/images/jumper.jpeg'
+);
 
 new Kitten(
-  'Jumper',
-  'Jumper the cat is super sweet!',
-  ['cuddling', 'bitch ass', 'cat nap'],
-  'images/jumper.jpeg'
+  'Serena ',
+  'Serena is a sweet kitty',
+  ['string', 'feathers', 'soft beds'],
+  'js/images/serena.jpeg'
 );
-// 1.) create element
-// let article = document.createElement('article');
 
-// // 2. or 3.) give it content
-// // 2. or 3.) append it to the DOM
+new Kitten(
+  'Frankie ',
+  'Frankie is available for adoption',
+  ['naps', 'treats', 'cat tree'],
+  'js/images/frankie.jpeg'
+);
+//console.log(kittenArray);
 
+Kitten.prototype.renderTableRow = function () {
+  let tr = document.createElement('tr');
+  kittenTable.appendChild(tr);
+  let tdName = document.createElement('td');
+  tdName.textContent = this.name;
+  tr.appendChild(tdName);
+  for (let i = 0; i < this.like.length; i++) {
+    let td = document.createElement('td');
+    td.textContent = this.like[i];
+    tr.appendChild(td);
+  }
+};
 
-// // h3
-// let h3 = document.createElement('h3');
-// h3.textContent = 'Jumper';
-// article.appendChild(h3);
-// // img
-// let img = document.createElement('img');
-// img.src = 'js/images/jumper.jpeg';
-// img.alt = 'Jumper the cat is super sweet!';
-// article.appendChild(img);
-
-// // p
-// let p = document.createElement('p');
-// p.textContent = 'Jumper the cat is super sweet!';
-// article.appendChild(p);
-
-// // ul
-// let ul = document.createElement('ul');
-// article.appendChild(ul);
-
+// frankie.render();
 // jumper.render();
+// serena.render();
 
+// jumper.renderTableRow();
+// frankie.renderTableRow();
+// serena.renderTableRow();
 
-// profileContainer.appendChild(article);
+for (let i = 0; i < kittenArray.length; i++) {
+  kittenArray[i].render();
+  kittenArray[i].renderTableRow();
+}
 
-
-
-
-
-// const profileContainer2 = document.getElementById('kittenProfiles2');
-
-// let frankie = {
-//   name: 'Frankie',
-//   about: 'frankie rocks',
+// let jumper = {
+//   name: 'Jumper',
+//   about: 'Jumper the cat is super sweet!',
 //   cutenessIndex: 10,
 //   like: [
-//     'pizza',
-//     'tacos',
-//     'naps',
-//     'cats'
+//     'cuddling',
+//     'lazer pointer',
+//     'cat nap'
 //   ],
+//   src: 'images/jumper.jpeg',
+//   listId: `${this.name}Likes`,
 //   render: function () {
-//     for (let i in this.like) {
+//     // 1.) create element
+//     let article = document.createElement('article');
+
+//     // 2. or 3.) give it content
+//     // 2. or 3.) append it to the DOM
+//     profileContainer.appendChild(article);
+
+//     // h3
+//     let h3 = document.createElement('h3');
+//     h3.textContent = this.name;
+//     article.appendChild(h3);
+
+//     // p
+//     let p = document.createElement('p');
+//     p.textContent = this.about;
+//     article.appendChild(p);
+
+//     // ul
+//     let ul = document.createElement('ul');
+//     ul.id = this.listId;
+//     article.appendChild(ul);
+
+//     // img
+//     let img = document.createElement('img');
+//     img.src = this.src;
+//     img.alt = this.about;
+//     article.appendChild(img);
+
+//     this.renderList();
+//   },
+//   renderList: function () {
+//     const kittenLikes = document.getElementById(this.listId);
+//     for (let i = 0; i < this.like.length; i++) {
 //       // li
 //       let li = document.createElement('li');
 //       li.textContent = `${this.name} likes ${this.like[i]}`;
-//       ul2.appendChild(li);
+//       kittenLikes.appendChild(li);
 //     }
-//   }
+//   },
 // };
-
-
-// let article2 = document.createElement("article")
-
-
-// let h32 = document.createElement('h3');
-// h32.textContent = 'Frankie';
-// article2.appendChild(h32);
-// // img
-// let img2 = document.createElement('img');
-// img2.src = 'js/images/jumper.jpeg';
-// img2.alt = 'Jumper the cat is super sweet!';
-// article2.appendChild(img2);
-
-// let p2 = document.createElement('p');
-// p2.textContent = 'Frankie the cat is super sweet!';
-
-// article2.appendChild(p);
-
-// let ul2 = document.createElement('ul')
-// article2.appendChild(ul2)
-
-// frankie.render();
-
-// profileContainer2.appendChild(article2)
-
-
-// let profileContainer3 = document.getElementById('kittenProfiles3')
-
-// let serena = {
-//   name: "Serena",
-//   about: 'Good cat',
-//   cutenessIndex: 10,
-//   like: [
-//     'hotdogs',
-//     'burgers',
-//     'hats',
-//     'motorcycles'
-//   ],
-
-//   render: function () {
-//     for (let i in this.like) {
-//       let li = document.createElement('li');
-//       li.textContent = `${this.name} like ${this.like[i]}`;
-//       ul3.appendChild(li);
-//     }
-//   }
-// }
-
-
-// let article3 = document.createElement('article')
-
-// let h33 = document.createElement('h3')
-// h33.textContent = serena.name
-// article3.appendChild(h33)
-
-// let img3 = document.createElement('img');
-// img3.src = 'js/images/serena.jpeg';
-// img3.alt = 'This cat is named Serena';
-// article3.appendChild(img3);
-
-
-// let p3 = document.createElement('p')
-// p3.textContent = serena.about;
-// article3.appendChild(p3)
-
-// let rating3 = document.createElement('p');
-// rating3.textContent = `Serena's cuteness level is ${serena.cutenessIndex}`;
-// article3.appendChild(rating3)
-
-// let ul3 = document.createElement('ul');
-// article3.appendChild(ul3)
-
-// serena.render()
-
-// profileContainer3.appendChild(article3)
